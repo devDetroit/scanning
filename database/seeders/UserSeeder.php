d830e4eaa42fd98a6ea1d05b79c06259d877db13
rortegaa <41353712+rortegaa@users.noreply.github.com>
2022-05-24 17:34:53 -0500

    usuario agregar

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,10 +16,29 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'user' => 'test',
-            'complete_name' => 'test test',
-            'email' => 'test@test.com',
-            'password' => Hash::make('test'),
+            'user' => 'admin',
+            'complete_name' => 'administrator',
+            'email' => 'dortega@detroitaxle.com',
+            'password' => Hash::make('admin'),
+        ]);
+
+        User::create([
+            'user' => 'station1',
+            'complete_name' => 'Station 1',
+            'password' => Hash::make('station1'),
+        ]);
+
+        User::create([
+            'user' => 'station2',
+            'complete_name' => 'Station 2',
+            'password' => Hash::make('station2'),
+        ]);
+
+        User::create([
+            'user' => 'jgonzalez',
+            'complete_name' => 'Jesus Gonzalez',
+            'email' => 'jesusgonzalez@detroitaxle.com',
+            'password' => Hash::make('jgonzalez'),
         ]);
     }
 }
